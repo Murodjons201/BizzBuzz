f6fc1ec50b9d118b031bfd9bcae40b27a3711e92
Murodjon Sayidov <sayidovmurodjon201@gmail.com>
2021-12-30 20:50:00 +0500

    initial-commit

--- js/script.js
+++ js/script.js
@@ -2,25 +2,37 @@ let elForm = document.querySelector(".form");
 let elFormInput = document.querySelector(".form__input");
 let elResult = document.querySelector(".form__result");
 
-elForm.addEventListener('submit', function(evt) {
-    evt.preventDefault();
+function fizzBuzz(number) {
+  if (number % 15 === 0) {
+    return "FizzBuzz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else {
+    return number - 0;
+  }
+}
 
-    let inputValue = elFormInput.value;
+elForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
 
-    function fizzBuzz(number) {
+  let inputValue = elFormInput.value;
 
-        if((number % 15) === 0) {
-            return "FizzBuzz";
-        } else if ((number % 5) === 0) {
-            return "Buzz";
-        } else if((number % 3) === 0) {
-            return "Fizz";
-        } else {
-            return inputValue;
-        }
+  if (inputValue < 0 || isNaN(inputValue)) {
+    elResult.textContent = "Iltimos 0 dan katta raqam kiriting!";
+    elResult.classList.remove("bg-light");
+    elResult.classList.add("bg-primary");
+    elResult.classList.add("text-light");
 
-    }
+    return;
+  } else {
+    elResult.classList.remove("bg-primary");
+    elResult.classList.remove("text-light");
+    elResult.classList.add("bg-light");
+    elResult.classList.add("text-pimery");
+  }
 
-    elResult.textContent = fizzBuzz(inputValue);
-    console.log(fizzBuzz(inputValue));
-})
+  elResult.textContent = fizzBuzz(inputValue);
+  console.log(fizzBuzz(inputValue));
+});
